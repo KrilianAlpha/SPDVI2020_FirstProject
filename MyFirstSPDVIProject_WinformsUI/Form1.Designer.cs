5ff55a8bd190ccce04611da322cd94a7d46fc48c
Krilian <tank2114@gmail.com>
2020-10-07 19:54:06 +0200

    modified the text of the winforms button

--- MyFirstSPDVIProject_WinformsUI/Form1.Designer.cs
+++ MyFirstSPDVIProject_WinformsUI/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Nimbus";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
